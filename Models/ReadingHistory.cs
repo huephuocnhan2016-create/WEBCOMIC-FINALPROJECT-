285d2971e777e78be7a00c7cf26edc8a76b9eec9
unknown <tuyetnhi2609>
2026-04-01 01:52:01 +0700

    Cập nhật thêm nội dung mới

---
+++ Models/ReadingHistory.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEBCOMIC_FINALPROJECT_.Models
+{
+    public class ReadingHistory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        public int MangaId { get; set; }
+
+        // Đổi ReadDate thành LastRead để khớp với code đang bị lỗi
+        public DateTime LastRead { get; set; } = DateTime.Now;
+
+        [ForeignKey("MangaId")]
+        public virtual Manga? Manga { get; set; }
+    }
+}
